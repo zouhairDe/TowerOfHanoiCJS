989b602f27d299685063b207baf299406cc01b26
Zouhair <121346419+zouhairDe@users.noreply.github.com>
2023-09-29 17:36:09 +0100

    Create TowerOfHanoi.js

---
+++ TowerOfHanoi.jsfunction towerOfHanoi(n, source, auxiliary, destination) {     if (n === 1) {         console.log(`Move disk 1 from ${source} to ${destination}`);         return;     }      towerOfHanoi(n - 1, source, destination, auxiliary);     console.log(`Move disk ${n} from ${source} to ${destination}`);     towerOfHanoi(n - 1, auxiliary, source, destination); }  const numDisks = 3; /TowerOfHanoi.js
@@ -0,0 +1,14 @@
+function towerOfHanoi(n, source, auxiliary, destination) {
+    if (n === 1) {
+        console.log(`Move disk 1 from ${source} to ${destination}`);
+        return;
+    }
+
+    towerOfHanoi(n - 1, source, destination, auxiliary);
+    console.log(`Move disk ${n} from ${source} to ${destination}`);
+    towerOfHanoi(n - 1, auxiliary, source, destination);
+}
+
+const numDisks = 3; // You can change this to the number of disks you want to solve for
+console.log(`Tower of Hanoi with ${numDisks} disks:`);
+towerOfHanoi(numDisks, 'A', 'B', 'C');
